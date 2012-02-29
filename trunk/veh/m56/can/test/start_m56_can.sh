b86e377291aa6f8f5df95c26bf58aaf85a711120
jspring <jspring@path.berkeley.edu>
2012-02-29 23:40:44 +0000

    M    test/start_m56_can.sh 	Changed shell to bash, and do a ps check instead of looking at return 	value (a backgrounded process always returns 0)

--- trunk/veh/m56/can/test/start_m56_can.sh
+++ trunk/veh/m56/can/test/start_m56_can.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 /bin/ps -ef | grep db_slv | grep -v grep
 
@@ -10,9 +10,14 @@ fi
 killall m56_can
 killall komodo_db_write
 sleep 1
-pushd /home/path/sens/can2usb/komodo/files/komodo-api-linux-i686-v1.00/c
+cd /home/path/sens/can2usb/komodo/files/komodo-api-linux-i686-v1.00/c
 echo Starting komodo_db_write...
 lnx/komodo_db_write -v &
-popd
+sleep 1
+if [[ `ps -ef | grep -v grep | grep komodo_db_write` == '' ]]
+then
+	echo $0: komodo_db_write startup error. Exiting.
+	exit 1
+fi
 sleep 1
 /home/das3/veh/m56/can/src/lnx/m56_can -c &
