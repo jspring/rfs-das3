2fe5c0943810d061da560a47aa2de81bdfc04f35
jspring <jspring@path.berkeley.edu>
2012-03-28 20:50:00 +0000

    M    test/start_cacc3.sh 	Moved all M56 software that was in cacc3 to here, and therefore 	changed start_cacc3.sh accordingly

--- trunk/veh/m56/test/start_cacc3.sh
+++ trunk/veh/m56/test/start_cacc3.sh
@@ -23,20 +23,20 @@ fi
 fi
 fi
 
-tripdir=$(/home/cacc3/test/mknewtripdir.sh)
+tripdir=$(/home/das3/veh/m56/test/mknewtripdir.sh)
 echo "VEH $VEH tripdir $tripdir"
 
 /home/das3/veh/m56/test/setports.sh
 /home/path/sens/gps/examples/lnx/gpssetdate < /dev/ttyS0 &
 /home/das3/startup/killsoon.sh -x gpssetdate -t 120 >$tripdir/killsoon.log
 
-/home/cacc3/test/stop_cacc3.sh
+/home/das3/veh/m56/test/stop_cacc3.sh
 sleep 2
 echo Starting db_slv...
 /home/path/db/lnx/db_slv &
 sleep 1
 echo Starting cacc3_create...
-/home/cacc3/veh/lnx/cacc3_create &
+/home/das3/veh/m56/src/lnx/cacc3_create &
 sleep 1 
 
 cd /home/path/sens/can2usb/komodo/files/komodo-api-linux-i686-v1.00/c
@@ -57,9 +57,9 @@ echo Starting gpsdb...
 /home/path/sens/gps/examples/lnx/gpsdb -d 1 < /dev/ttyS0 >$tripdir/gpsdb.log 2>&1 &
 sleep 1
 echo Starting wrfiles_cacc3 with tripdir $tripdir vehicle prefix $VEH...
-/home/cacc3/veh/lnx/wrfiles_cacc3 -m 2 -t 50 -d $tripdir -c $VEH -i 1>$tripdir/wrfiles_cacc3.log 2>$tripdir/wrfiles_cacc3.err &
+/home/das3/veh/m56/src/lnx/wrfiles_cacc3 -m 2 -t 50 -d $tripdir -c $VEH -i 1>$tripdir/wrfiles_cacc3.log 2>$tripdir/wrfiles_cacc3.err &
 sleep 2
 echo Starting video with tripdir $tripdir vehicle prefix $VEH...
 /home/capath/video/videorecorder/video -0 $VEH -qt -f "/big/data" -p $tripdir -b 2000000 -c >$tripdir/video.log 2>$tripdir/video.err &
-/home/cacc3/test/getstats.sh >$tripdir/stats.log &
+/home/das3/veh/m56/test/getstats.sh >$tripdir/stats.log &
 /home/das3/veh/m56/lcd_hw/lcd_io.sh $1 &
