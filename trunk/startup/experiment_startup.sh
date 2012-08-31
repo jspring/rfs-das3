6329ed4a8e1e5ee0b6158b2dd610445c9bd6f357
jspring <jspring@path.berkeley.edu>
2012-08-31 20:15:58 +0000

    M    startup/experiment_startup.sh 	Separated block of code for configuring the vehicle startups from 	that of the experiment startups.  The vehicle startup just creates 	the library links and sets the character for the vehicle files. The 	experiment startup creates the library links for the experiment and 	starts wrfiles_das3.
    	Changed "sudo ldconfig" to "sudo ldconfig -n /home/das3/src". For
    	some reason, ldconfig wasn't picking up the "/home/das3/src" 
    	directory from /etc/ld.so.conf.

--- trunk/startup/experiment_startup.sh
+++ trunk/startup/experiment_startup.sh
@@ -27,40 +27,96 @@ then
 	exit 0
 fi
 
+if [[ $CARTYPE =~ "altima" ]]
+then
+	# Set vehicle character for Altimas
+	if [[ $CARTYPE =~ "grey" ]]
+	then
+		VEH=g
+	else if [[ $CARTYPE =~ "silver" ]]
+	then
+                        VEH=h
+	else 
+		echo CARTYPE $CARTYPE not found
+		exit 1
+	fi
+	fi
+
+	# Create library links for altima
+	ls -l /home/das3/src/libveh_tables.so.1 | grep libaltima_tables.so.1.0
+	x=$?
+	ls -L /home/das3/src/libveh_tables.so
+	y=$?
+	if [[ $x != 0 || $y != 0 ]]
+	then
+		echo No Altima vehicle library links - creating them now
+		rm /home/das3/src/libveh*
+		ln -sf /home/das3/veh/altima/src/lnx/libaltima_tables.so.1.0 /home/das3/src/libveh_tables.so.1
+		ln -sf /home/das3/src/libveh_tables.so.1 /home/das3/src/libveh_tables.so
+		sudo ldconfig -n /home/das3/src
+	fi
+fi
+
+if [[ $CARTYPE =~ "audi" ]]
+then
+	if [[ $CARTYPE =~ "silver" ]]
+	then
+		VEH=j
+	else
+		VEH=k
+	fi
+
+	echo "$0: No Standalone Data Logging Script Specified for CARTYPE=$CARTYPE"
+	exit 1
+fi
+
+if [[ $CARTYPE =~ "m56" ]]
+then
+	if [[ $CARTYPE =~ "dnc304" ]]
+	then
+		VEH=m
+
+	else if [[ $CARTYPE =~ "pro4" ]]
+	then
+                        VEH=n
+
+	else if [[ $CARTYPE =~ "dne491" ]]
+	then
+                        VEH=o
+
+	else if [[ $CARTYPE =~ "dne596" ]]
+	then
+                        VEH=p
+	else 
+		echo CARTYPE $CARTYPE not found
+		exit 1
+	fi
+	fi
+	fi
+	fi
+	# Create library links for standalone m56
+	ls -l /home/das3/src/libveh_tables.so.1 | grep libm56_tables.so.1.0
+	x=$?
+	ls -L /home/das3/src/libveh_tables.so
+	y=$?
+	if [[ $x != 0 || $y != 0 ]]
+	then
+		rm /home/das3/src/libveh*
+		ln -sf /home/das3/veh/m56/src/lnx/libm56_tables.so.1.0 /home/das3/src/libveh_tables.so.1
+		ln -sf /home/das3/src/libveh_tables.so.1 /home/das3/src/libveh_tables.so
+		sudo ldconfig -n /home/das3/src
+	fi
+
+echo VEH $VEH
+fi
+
 # STANDALONE starts the wrtfiles for the vehicle type contained in /home/das3
 if [[ $EXPERIMENT =~ "standalone" ]]
 then
 	echo "Starting $CARTYPE Standalone Data Logging..."
-	
+
 	if [[ $CARTYPE =~ "altima" ]]
 	then
-		# Set vehicle character for Altimas
-		if [[ $CARTYPE =~ "grey" ]]
-		then
-			VEH=g
-		else if [[ $CARTYPE =~ "silver" ]]
-		then
-                        VEH=h
-		else 
-			echo CARTYPE $CARTYPE not found
-			exit 1
-		fi
-		fi
-
-		# Create library links for standalone altima
-		ls -l /home/das3/src/libveh_tables.so.1 | grep libaltima_tables.so.1.0
-		x=$?
-		ls -L /home/das3/src/libveh_tables.so
-		y=$?
-		if [[ $x != 0 || $y != 0 ]]
-		then
-			echo No Altima vehicle library links - creating them now
-			rm /home/das3/src/libveh*
-			ln -sf /home/das3/veh/altima/src/lnx/libaltima_tables.so.1.0 /home/das3/src/libveh_tables.so.1
-			ln -sf /home/das3/src/libveh_tables.so.1 /home/das3/src/libveh_tables.so
-			sudo ldconfig
-		fi
-
 		ls -l /home/das3/src/libexpt_tables.so.1 | grep libexpt_tables.so.1.0
 		x=$?
 		ls -L /home/das3/src/libexpt_tables.so
@@ -71,7 +127,7 @@ then
 			rm /home/das3/src/libexpt*
 			ln -sf /home/das3/src/lib_templates/lnx/libexpt_tables.so.1.0 /home/das3/src/libexpt_tables.so.1
 			ln -sf /home/das3/src/libexpt_tables.so.1 /home/das3/src/libexpt_tables.so
-			sudo ldconfig
+			sudo ldconfig -n /home/das3/src
 		fi
 		
 		# Start standalone altima data logging
@@ -95,41 +151,6 @@ then
 
 	if [[ $CARTYPE =~ "m56" ]]
 	then
-		if [[ $CARTYPE =~ "dnc304" ]]
-		then
-			VEH=m
-
-		else if [[ $CARTYPE =~ "pro4" ]]
-		then
-                        VEH=n
-
-		else if [[ $CARTYPE =~ "dne491" ]]
-		then
-                        VEH=o
-
-		else if [[ $CARTYPE =~ "dne596" ]]
-		then
-                        VEH=p
-		else 
-			echo CARTYPE $CARTYPE not found
-			exit 1
-		fi
-		fi
-		fi
-		fi
-		# Create library links for standalone m56
-		ls -l /home/das3/src/libveh_tables.so.1 | grep libm56_tables.so.1.0
-		x=$?
-		ls -L /home/das3/src/libveh_tables.so
-		y=$?
-		if [[ $x != 0 || $y != 0 ]]
-		then
-			rm /home/das3/src/libveh*
-			ln -sf /home/das3/veh/m56/src/lnx/libm56_tables.so.1.0 /home/das3/src/libveh_tables.so.1
-			ln -sf /home/das3/src/libveh_tables.so.1 /home/das3/src/libveh_tables.so
-			sudo ldconfig
-		fi
-
 		ls -l /home/das3/src/libexpt_tables.so.1 | grep libexpt_tables.so.1.0
 		x=$?
 		ls -L /home/das3/src/libexpt_tables.so
@@ -140,7 +161,7 @@ then
 			rm /home/das3/src/libexpt*
 			ln -sf /home/das3/src/lib_templates/lnx/libexpt_tables.so.1.0 /home/das3/src/libexpt_tables.so.1
 			ln -sf /home/das3/src/libexpt_tables.so.1 /home/das3/src/libexpt_tables.so
-			sudo ldconfig
+			sudo ldconfig -n /home/das3/src
 		fi
 		
 #		/home/das3/src/lnx/wrfiles_das3 -m 2 -t 50 -d $TRIPDIR -c $VEH -i -AD 1>$TRIPDIR/wrfiles_m56.log 2>$TRIPDIR/wrfiles_m56.err &
@@ -153,20 +174,54 @@ fi
 # NTMM starts Networked Traveler-Mobile Millennium Experiment
 if [[ $EXPERIMENT =~ "ntmm" ]]
 then
-
-	echo "Starting NTMM Experiment & Data Logging..."
-	/home/ntmm/test/start.sh $CARTYPE $EXPERIMENT $TRIPDIR
-	exit 0
-
+	if [[ $CARTYPE =~ "altima" ]]
+	then
+		ls -l /home/das3/src/libexpt_tables.so.1 | grep libalert_tables.so.1.0
+		x=$?
+		ls -L /home/das3/src/libexpt_tables.so
+		y=$?
+		if [[ $x != 0 || $y != 0 ]]
+		then
+			echo No CACC3 library links - creating them now
+			rm /home/das3/src/libexpt*
+			ln -sf /home/ntmm/src/lnx/libalert_tables.so.1.0 /home/das3/src/libexpt_tables.so.1
+			ln -sf /home/das3/src/libexpt_tables.so.1 /home/das3/src/libexpt_tables.so
+			sudo ldconfig -n /home/das3/src
+		fi
+		echo "Starting NTMM Experiment & Data Logging..."
+		/home/ntmm/test/start.sh $CARTYPE $EXPERIMENT $TRIPDIR
+		exit 0
+	else
+		echo "$CARTYPE CARTYPE not correct for $EXPERIMENT EXPERIMENT
+		exit 1
+	fi
 fi
 
 # CACC3 starts Nissan CACC3 Experiment
 if [[ $EXPERIMENT =~ "cacc3" ]]
 then
+	if [[ $CARTYPE =~ "m56" ]]
+	then
+		ls -l /home/das3/src/libexpt_tables.so.1 | grep libcacc3_tables.so.1.0
+		x=$?
+		ls -L /home/das3/src/libexpt_tables.so
+		y=$?
+		if [[ $x != 0 || $y != 0 ]]
+		then
+			echo No CACC3 library links - creating them now
+			rm /home/das3/src/libexpt*
+			ln -sf /home/cacc3/src/lnx/libcacc3_tables.so.1.0 /home/das3/src/libexpt_tables.so.1
+			ln -sf /home/das3/src/libexpt_tables.so.1 /home/das3/src/libexpt_tables.so
+			sudo ldconfig -n /home/das3/src
+		fi
 
-	echo "Starting CACC3 Experiment & Data Logging..."
-	/home/das3/src/lnx/wrfiles_das3 -m 2 -t 50 -d $TRIPDIR -c $VEH -i 1>$TRIPDIR/wrfiles_m56.log 2>$TRIPDIR/wrfiles_m56.err &
-	exit 0
+		echo "Starting CACC3 Experiment & Data Logging..."
+		/home/das3/src/lnx/wrfiles_das3 -m 2 -t 50 -d $TRIPDIR -c $VEH -i 1>$TRIPDIR/wrfiles_m56.log 2>$TRIPDIR/wrfiles_m56.err &
+		exit 0
+	else
+		echo "$CARTYPE CARTYPE not correct for $EXPERIMENT EXPERIMENT
+		exit 1
+	fi
 fi
 
 # UNKNOWN EXPERIMENT
